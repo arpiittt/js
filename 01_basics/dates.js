3fc6a3cfb9fee901f688a78be758eab6586040d2
arpiittt <120245787+arpiittt@users.noreply.github.com>
2024-07-01 14:43:41 +0000

    dates representation

---
+++ 01_basics/dates.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
